bd7e60cd470a86d1207eea853a00be66c377f4f8
uday57081 <udaykiranreddy@gmail.com>
2026-09-13 16:14:29 +0530

    Initial commit - MBR My Best Raksha

---
+++ src/main/java/com/mbr/my_best_raksha/MyBestRakshaApplication.java
@@ -0,0 +1,13 @@
+package com.mbr.my_best_raksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBestRakshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBestRakshaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbr/my_best_raksha/MyBestRakshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbr.my_best_raksha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBestRakshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
